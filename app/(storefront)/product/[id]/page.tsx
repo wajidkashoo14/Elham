5f2b460bbcafc42904360d273fcb641ebe340315
wajid hussain kashoo <i.wajid.hussain@generalaeronautics.com>
2024-09-07 15:42:06 +0530

    completed product id page

--- app/(storefront)/product/[id]/page.tsx
+++ app/(storefront)/product/[id]/page.tsx
@@ -1,5 +1,8 @@
+import { Featuredproducts } from "@/app/components/storefront/FeaturedProducts";
 import { ImageSlider } from "@/app/components/storefront/ImageSlider";
 import prisma from "@/app/lib/db";
+import { Button } from "@/components/ui/button";
+import { ShoppingBag, StarIcon } from "lucide-react";
 import { notFound } from "next/navigation";
 
 async function getData(productId: string) {
@@ -30,8 +33,28 @@ export default async function ProductIdRoute({
   const data = await getData(params.id);
   return (
     <>
-      <div className="gird grid-cols-1 md:grid-cols-2 gap-6 items-start lg:gap-x-24 py-6">
-        <ImageSlider images={data.images}/>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-start lg:gap-x-24 py-6">
+        <ImageSlider images={data.images} />
+        <div>
+          <h1 className="text-3xl font-semibold tracking-tight text-gray-900">
+            {data.name}
+          </h1>
+          <p className="text-3xl mt-2 text-gray-900">{data.price}</p>
+          <div className="mt-3 flex items-center gap-1">
+            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
+            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
+            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
+            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
+            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
+          </div>
+          <p className="text-base text-gray-700 mt-6">{data.description}</p>
+          <Button size="lg" className="w-full mt-5">
+            <ShoppingBag className="mr-4 h-5 w-5 " /> Add to Cart
+          </Button>
+        </div>
+      </div>
+      <div className="mt-16">
+        <Featuredproducts />
       </div>
     </>
   );
