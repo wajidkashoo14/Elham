d14c4be0d5fac6bf7b5ce136d5da8ac2b94730ae
wajid hussain kashoo <i.wajid.hussain@generalaeronautics.com>
2024-09-27 00:27:29 +0530

    added about us page

--- app/(storefront)/aboutus/page.tsx
+++ app/(storefront)/aboutus/page.tsx
@@ -1,5 +1,36 @@
 function AboutRoute() {
-    return <h1>About us Route</h1>
+  return (
+    <div className="mt-8 ">
+      <div className="grid grid-cols-3 gap-4 max-w-5xl">
+        <div>
+          <h2>About us</h2>
+          <h2>Our Team</h2>
+          <h2>Press</h2>
+        </div>
+        <div className="col-span-2">
+          <h1 className="text-transform: uppercase text-3xl font-semibold mb-5">
+            about us
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum
+            doloremque corrupti repellendus id totam magni, mollitia et earum
+            necessitatibus cupiditate nobis voluptatem itaque minus ullam. Ad,
+            eius corrupti voluptas quidem, facere sed commodi doloremque
+            provident iste quia velit ex assumenda. Sed autem asperiores a!
+            Sapiente quos eos dignissimos possimus!
+          </p>
+          <p className="mt-8">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum
+            doloremque corrupti repellendus id totam magni, mollitia et earum
+            necessitatibus cupiditate nobis voluptatem itaque minus ullam. Ad,
+            eius corrupti voluptas quidem, facere sed commodi doloremque
+            provident iste quia velit ex assumenda. Sed autem asperiores a!
+            Sapiente quos eos dignissimos possimus!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default AboutRoute
+export default AboutRoute;
