fe5656eb271c92193b3c8394dcdde37cbe88d6df
wajid hussain kashoo <i.wajid.hussain@generalaeronautics.com>
2024-10-03 17:59:16 +0530

    footer change

--- app/components/storefront/Footer.tsx
+++ app/components/storefront/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
       <div className="pb-10">
         <div className="mx-auto container pt-20 lg:pt-44 flex flex-col items-center justify-center">
           <div className="text-black flex flex-col md:items-center f-f-l pt-3">
-            <h1 className="text-2xl font-black">Elham</h1>
+            <h1 className="text-2xl font-black">Elham 2024</h1>
             <div className="my-6 text-base text-color f-f-l">
               <ul className="md:flex items-center">
                 <li className=" md:mr-6 cursor-pointer pt-4 lg:py-0">About</li>
