e0f2e9f35dee00f541f43161c601ccee14acbf5f
wajid hussain kashoo <i.wajid.hussain@generalaeronautics.com>
2024-10-05 16:48:14 +0530

    navbar fix

---
+++ app/components/storefront/NavDropdown.tsx
@@ -0,0 +1,121 @@
+import { useState } from "react";
+import { ChevronDown } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
+interface DropdownOption {
+  href: string;
+  label: string;
+  desc: string;
+  image: string;
+}
+
+interface DropdownProps {
+  triggerText: string;
+  options: DropdownOption[];
+  align?: "left" | "right" | "center";
+}
+
+const Dropdown: React.FC<DropdownProps> = ({
+  triggerText,
+  options,
+  align = "right",
+}) => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const location = usePathname();
+
+  // Handle mouse events
+  const handleMouseEnter = () => setDropdownOpen(true);
+  const handleMouseLeave = () => setDropdownOpen(false);
+
+  // Align dropdown classes
+  const alignClass =
+    align === "left"
+      ? "left-0"
+      : align === "center"
+      ? "left-1/2 transform -translate-x-1/2"
+      : "right-0";
+
+  return (
+    <div
+      className="relative inline-block text-left"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {/* Trigger with animated arrow */}
+      <button
+        className={cn(
+            options.some((option) => option.href === location)
+              ? "bg-muted"
+              : "hover:bg-muted hover:bg-opacity-75",
+            "flex items-center justify-center hover:bg-gray-100 rounded-lg w-full p-2"
+          )}
+        aria-expanded={dropdownOpen}
+      >
+        <span
+          className={cn(
+            options.some((option) => option.href === location)
+              ? "bg-muted"
+              : "hover:bg-muted hover:bg-opacity-75",
+            "flex items-center justify-center font-medium gap-1 hover:bg-gray-100 rounded-lg w-full px-1"
+          )}
+        >
+          {triggerText}
+        </span>
+        <ChevronDown
+          className={`transition-transform duration-300 ${
+            dropdownOpen ? "rotate-180" : "rotate-0"
+          }`}
+          size={16}
+        />
+      </button>
+
+      {/* Dropdown Menu */}
+      {dropdownOpen && (
+        <div
+          className={`absolute ${alignClass} md:w-[250px] cursor-pointer mt-1 w-48 bg-white rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 z-10 p-2`}
+        >
+          {options.map((option, index) => (
+            <div>
+              <Link
+                key={index}
+                href={option.href}
+                className={cn(
+                  location === option.href
+                    ? "bg-muted"
+                    : "hover:bg-muted hover:bg-opacity-75",
+                  "flex items-center justify-center gap-3 hover:bg-gray-100 rounded-lg w-full py-3 px-1"
+                )}
+              >
+                <div>
+                  <Image
+                    src={option?.image}
+                    alt={`${option.label}-icon`}
+                    width={50}
+                    height={50}
+                    objectFit="cover"
+                  />
+                </div>
+                <div
+                  className={cn(
+                    location === option.href
+                      ? "bg-muted"
+                      : "hover:bg-muted hover:bg-opacity-75",
+                    " hover:text-gray-900 font-medium w-full "
+                  )}
+                >
+                  {option.label}
+                  <p className="text-xs w-full text-gray-500">{option.desc}</p>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;
--- app/components/storefront/NavbarLinks.tsx
+++ app/components/storefront/NavbarLinks.tsx
@@ -2,18 +2,19 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Dropdown from "./NavDropdown";
 
 export const navbarLinks = [
-  {
-    id: 0,
-    name: "Home",
-    href: "/",
-  },
-  {
-    id: 1,
-    name: "ShopOnline",
-    href: "/products/all",
-  },
+  // {
+  //   id: 0,
+  //   name: "Home",
+  //   href: "/",
+  // },
+  // {
+  //   id: 1,
+  //   name: "Shop",
+  //   href: "/products/all",
+  // },
   // {
   //   id: 1,
   //   name: "All Products",
@@ -29,11 +30,11 @@ export const navbarLinks = [
   //   name: "Clothing",
   //   href: "/products/clothing",
   // },
-  {
-    id: 3,
-    name: "Our Story",
-    href: "/products/clothing",
-  },
+  // {
+  //   id: 3,
+  //   name: "Our Story",
+  //   href: "/products/clothing",
+  // },
   {
     id: 4,
     name: "Journal",
@@ -46,13 +47,56 @@ export const navbarLinks = [
   },
 ];
 
+const shopDropdownOptions = [
+  {
+    href: "/products/clothing",
+    label: "Clothing",
+    desc: "Handcrafted clothing, made with care for a truly unique style.",
+    image: "/clothing-icon.svg",
+  },
+  {
+    href: "/products/handicrafts",
+    label: "Crafted Goods",
+    desc: "Beautifully crafted goods, designed with care for a unique touch.",
+    image: "/crafted-goods.png",
+  },
+  {
+    href: "/products/all",
+    label: "Kashmir Collection",
+    desc: "Exquisite Kashmiri paper mache, crafted by hand with tradition and care.",
+    image: "/draw-icon.png",
+  },
+];
+const ourJourneyDropdownOptions = [
+  {
+    href: "/founder",
+    label: "About the Founder",
+    desc: "A brief introduction/my background + vision + motivations",
+    image: "/clothing-icon.svg",
+  },
+  {
+    href: "/artisans",
+    label: "Meet Our Artisans",
+    desc: "Highlighting the skills, stories of the artisans we work with",
+    image: "/crafted-goods.png",
+  },
+];
+
 export function NavbarLinks() {
   const location = usePathname();
   return (
-    <div
-      className="hidden md:flex justify-center items-center gap-x-2
-    "
-    >
+    <div className="hidden md:flex justify-center items-center gap-x-3">
+      <Link
+        href="/"
+        className={cn(
+          location === "/" ? "bg-muted" : "hover:bg-muted hover:bg-opacity-75",
+          "group p-2 font-medium rounded-md color-[#213869]"
+        )}
+      >
+        Home
+      </Link>
+      <Dropdown triggerText="Shop" options={shopDropdownOptions} />
+      <Dropdown triggerText="Our Journey" options={ourJourneyDropdownOptions} />
       {navbarLinks.map((item) => (
         <Link
           href={item.href}
@@ -61,7 +105,7 @@ export function NavbarLinks() {
             location === item.href
               ? "bg-muted"
               : "hover:bg-muted hover:bg-opacity-75",
-            "group p-2 front-medium rounded-md color-[#213869]"
+            "group p-2 font-medium rounded-md color-[#213869]"
           )}
         >
           {item.name}
