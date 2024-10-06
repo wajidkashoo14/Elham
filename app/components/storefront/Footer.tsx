d6f850fa441cd7fa4a3bab98a501aa42310589ed
wajid hussain kashoo <i.wajid.hussain@generalaeronautics.com>
2024-10-07 00:02:51 +0530

    code fix

--- app/components/storefront/Footer.tsx
+++ app/components/storefront/Footer.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import {
@@ -14,7 +15,13 @@ export default function Footer() {
       <div className="pb-10 w-full px-14 min-h-[60vh]">
         <div className=" container lg:pt-44 flex items-start justify-between max-w-full mb-10">
           <div className="w-1/3">
-            <h1 className="text-xl font-medium">Elham</h1>
+            <Image
+              src="/primarylogo1.jpg"
+              alt="Logo"
+              height={70}
+              width={200}
+              className="h-[30px] w-[150px] object-cover -ml-[5]"
+            />
             <p className="text-xm text-gray-700 mt-3 text-left">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
               cumque qui reiciendis harum molestias quae possimus ipsum,
@@ -51,7 +58,11 @@ export default function Footer() {
                 >
                   All Products
                 </Link>
-                <Link href="#faq" id="faq" className="cursor-pointer pt-4 lg:py-0">
+                <Link
+                  href="#faq"
+                  id="faq"
+                  className="cursor-pointer pt-4 lg:py-0"
+                >
                   FAQ
                 </Link>
               </ul>
@@ -61,12 +72,6 @@ export default function Footer() {
             <h1 className="text-xl font-medium">About us</h1>
             <div className="my-3 text-base text-color">
               <ul className="md:flex flex-col gap-1 items-center justify-center text-[15px] text-gray-700">
-                <Link
-                  href="/press-coverage"
-                  className="cursor-pointer pt-4 lg:py-0"
-                >
-                  Press Coverage
-                </Link>
                 <Link
                   href="/privacy-policy"
                   className="cursor-pointer pt-4 lg:py-0"
@@ -82,6 +87,13 @@ export default function Footer() {
                 <Link href="/journal" className="cursor-pointer pt-4 lg:py-0">
                   Journal
                 </Link>
+                <Link
+                  href="https://www.google.com/"
+                  className="cursor-pointer pt-4 lg:py-0"
+                  target="_blank"
+                >
+                  Press
+                </Link>
               </ul>
             </div>
           </div>
@@ -92,17 +104,23 @@ export default function Footer() {
                 Get early info about our latest products
               </p>
               <div className="flex items-center gap-2">
-                <Link href="/">
-                  <FaFacebookSquare className="w-6 h-6 !bg-[#213869] text-white" />
+                <Link href="https://www.facebook.com/elham.fr/" target="_blank">
+                  <FaFacebookSquare className="w-6 h-6 text-[#213869]" />
                 </Link>
-                <Link href="/">
-                  <FaInstagramSquare className="w-6 h-6 !bg-[#213869] text-white" />
+                <Link
+                  href="https://www.instagram.com/shopelham.fr?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+                  target="_blank"
+                >
+                  <FaInstagramSquare className="w-6 h-6 text-[#213869]" />
                 </Link>
-                <Link href="/">
-                  <FaLinkedin className="w-6 h-6 !bg-[#213869] text-white" />
+                <Link
+                  href="https://www.linkedin.com/company/elham-fr?trk=profile-position"
+                  target="_blank"
+                >
+                  <FaLinkedin className="w-6 h-6 text-[#213869]" />
                 </Link>
-                <Link href="/">
-                  <FaSquarePinterest className="w-6 h-6 !bg-[#213869] text-white" />
+                <Link href="/" target="_blank">
+                  <FaSquarePinterest className="w-6 h-6 text-[#213869]" />
                 </Link>
               </div>
               <div className="flex items-center gap-2">
