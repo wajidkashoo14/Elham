bacef9d21985103a5942b2d23826553e45ee684e
wajid hussain kashoo <i.wajid.hussain@generalaeronautics.com>
2024-10-29 01:34:42 +0530

    code fiz

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 import { withUt } from "uploadthing/tw";
 
 const config = {
-  darkMode: ["class"], // Changed from ["class"] to "class"
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
