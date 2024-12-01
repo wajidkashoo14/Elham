708d9dbab1e25e1ef4e01a991c82b531970fff87
wajid hussain kashoo <i.wajid.hussain@generalaeronautics.com>
2024-12-01 11:54:22 +0530

    fixed video

--- app/components/storefront/Hero.tsx
+++ app/components/storefront/Hero.tsx
@@ -4,16 +4,17 @@ const Hero = () => {
   return (
     <div className="relative w-full h-screen rounded-xl overflow-hidden bg-transparent">
       <div className="absolute top-0 left-0 w-full h-full">
-        <video
-          className="w-full h-full object-cover object-center"
-          autoPlay
-          muted
-          loop
-          playsInline
-        >
-          <source src="/video.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+        <div className="rounded-lg">
+          <video
+            className="w-full aspect-[4/3] object-cover object-center rounded-lg"
+            autoPlay
+            muted
+            loop
+            playsInline
+          >
+            <source src="/video.mp4" type="video/mp4" />
+          </video>
+        </div>
       </div>
       <div className="relative z-10 flex items-center justify-center h-full bg-transparent bg-opacity-80">
         <h1 className="text-white text-4xl md:text-6xl font-bold opacity-80">
